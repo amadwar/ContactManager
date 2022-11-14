7d24f39ea66c87aa94a6fbe752155562af7e0faf
amadwar <118168373+amadwar@users.noreply.github.com>
2022-11-14 13:09:49 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import React, { useState } from 'react';
+
+
+function AddPersonForm(props) {
+  const [ person, setPerson ] = useState('');
+    
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+    
+  function handleSubmit(e) {
+    if(person !== '') {
+      props.handleSubmit(person);
+      setPerson('');
+    }
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+        placeholder="Add new contact" 
+        onChange={handleChange} 
+        value={person} />
+      <button type="submit">Einfügen</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <PeopleList data={contacts} />
+    </div>
+  );
+}
+
+
+
+
+
+
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+
+function App() {
+  return (
+    <div>
+      <header className='App-header'> 
+        <h1>Contact Manager</h1> 
+      </header>
+   
+      
+      <ContactManager data={contacts} />
+      
+       </div>
+  );
+}
+
+
+
+
+
+
+export default App;
+
